2904b0acf0b5d50bf85df0b0bc27750b92bf8482
shuvostack <shuvostack@gmail.com>
2026-04-30 22:38:45 +0600

    added api in client folder

--- client/src/app/contact/page.js
+++ client/src/app/contact/page.js
@@ -35,7 +35,7 @@ export default function ContactPage() {
 
       // send request to backend api
       const res = await axios.post(
-        "https://orbital-backend-9y6q.onrender.com/api/contact",
+        "/api/contact",
         payload,
       );
 
